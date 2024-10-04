334ceac32437f19050eb917b83557c16db50aff8
Yashovardhan-Tripathi <yashovardhan0001@gmail.com>
2024-10-04 22:16:27 +0530

    updating

--- Projects/try.cpp
+++ Projects/try.cpp
@@ -14,13 +14,13 @@ public:
         cout << "Sum of Two numbers are : " << num_1 + num_2;
       
     }
-     int get_Num1(void)
+     int get_Number1(void)
     {
         cout << "Enter your First number for Addition : " << endl;
         cin >> num_1;
     }
 
-    int get_Num2(void)
+    int get_Number2(void)
     {
         cout << "Enter your Second number for Addition : " << endl;
         cin >> num_2;
@@ -30,8 +30,8 @@ int main()
 {
     float num_1, num_2;
     Calsi Calculator;
-    Calculator.get_Num1();
-    Calculator.get_Num2();
+    Calculator.get_Number1();
+    Calculator.get_Number2();
     Calculator.add();
 
     return 0;
