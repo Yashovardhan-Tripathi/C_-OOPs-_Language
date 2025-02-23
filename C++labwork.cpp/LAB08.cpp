a932897c5631b2094f1fcabc47e52a6d876a3ad1
Yashovardhan Tripathi <yashovardhan0001@gmail.com>
2025-02-23 19:56:31 +0530

    add

--- C++labwork.cpp/LAB08.cpp
+++ C++labwork.cpp/LAB08.cpp
@@ -1,70 +1,180 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-class Power
+class Person
 {
-    int N, Choice;
-    double N1, N2;
+protected:
+    string name;
+    int age;
 
 public:
-    inline double multi()
+
+    Person(string n, int a) : name(n), age(a) {}
+
+    virtual void display()
     {
-        cout << "Multiplication of two numbers " << N1 << " and " << N2 << " are " << N1 * N2;
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
     }
-    inline int cube()
+
+    virtual ~Person() {}
+};
+
+class Engineer : public Person
+{
+    string field;
+
+public:
+    Engineer(string n, int a, string f) : Person(n, a), field(f) {}
+
+    void display() override
     {
-        cout << "Cube of number " << N << " is " << N * N * N;
+        Person::display();
+        cout << "Profession: Engineer" << endl;
+        cout << "Field of Engineering: " << field << endl;
     }
-    inline int get_input()
+};
+class Doctor : public Person
+{
+    string specialization;
+
+public:
+
+    Doctor(string n, int a, string spec) : Person(n, a), specialization(spec) {}
+
+    void display() override
     {
-        cout << "Enter the value for number " << endl;
-        cin >> N;
+        Person::display();
+        cout << "Profession: Doctor" << endl;
+        cout << "Specialization: " << specialization << endl;
     }
-    inline double get_inp()
+};
+class Student : public Person
+{
+    string major;
+
+public:
+    Student(string n, int a, string m) : Person(n, a), major(m) {}
+
+    void display() override
     {
-        cout << "Enter the value for number " << endl;
-        cin >> N1 >> N2;
-    }
-    inline void Option()
-    {
-        cout << "1.Multiplication of two Numbers " << endl;
-        cout << "2.Cube of a Number " << endl;
-    }
-    int Choose(void)
-    {
-        cout << "Enter your operation from menu " << endl;
-        cin >> Choice;
-        return Choice;
+        Person::display();
+        cout << "Profession: Student" << endl;
+        cout << "Major: " << major << endl;
     }
 };
 
+class Laborer : public Person
+{
+    string jobType;
+
+public:
+
+    Laborer(string n, int a, string j) : Person(n, a), jobType(j) {}
+
+    void display() override
+    {
+        Person::display();
+        cout << "Profession: Laborer" << endl;
+        cout << "Type of Job: " << jobType << endl;
+    }
+};
+
+Person *createPerson(int choice)
+{
+    string name;
+    int age;
+
+    cout << "Enter name: ";
+    cin >> name;
+    cout << "Enter age: ";
+    cin >> age;
+
+    switch (choice)
+    {
+    case 1:
+    {
+        string field;
+        cout << "Enter field of engineering: ";
+        cin >> field;
+        return new Engineer(name, age, field);
+    }
+    case 2:
+    {
+        string specialization;
+        cout << "Enter specialization: ";
+        cin >> specialization;
+        return new Doctor(name, age, specialization);
+    }
+    case 3:
+    {
+        string major;
+        cout << "Enter major: ";
+        cin >> major;
+        return new Student(name, age, major);
+    }
+    case 4:
+    {
+        string jobType;
+        cout << "Enter type of job: ";
+        cin >> jobType;
+        return new Laborer(name, age, jobType);
+    }
+    default:
+        return nullptr;
+    }
+}
+
 int main()
 {
+
     cout << "Name:Yashovardhan Tripathi" << endl;
     cout << "PRN No.:230105231360" << endl;
     cout << "Division: D" << endl;
     cout << "Roll No.:14" << endl;
-    cout << "Experiment No:08" << endl;
+    cout << "Experiment No:08" << endl
+         << endl;
 
-    double N1, N2;
-    int N;
-    Power number;
-    number.Option();
-    int Ch = number.Choose();
+    vector<Person *> people;
+    int numPeople, choice;
 
-    switch (Ch)
+    cout << "How many people do you want to enter in the database? ";
+    cin >> numPeople;
+
+    for (int i = 0; i < numPeople; ++i)
     {
+        cout << "\nEnter details for person " << i + 1 << ":" << endl;
+        cout << "Choose profession:" << endl;
+        cout << "1. Engineer" << endl;
+        cout << "2. Doctor" << endl;
+        cout << "3. Student" << endl;
+        cout << "4. Laborer" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
 
-    case 1:
-        number.get_inp();
-        number.multi();
-        break;
-    case 2:
-        number.get_input();
-        number.cube();
-        break;
-    default:
-        cout << "Invalid Choice !!!!!!";
+        Person *person = createPerson(choice);
+        if (person)
+        {
+            people.push_back(person);
+        }
+        else
+        {
+            cout << "Invalid profession choice!" << endl;
+        }
     }
+
+    cout << "\nDisplaying details of all people:" << endl;
+    for (Person *person : people)
+    {
+        person->display();
+        cout << endl;
+    }
+    for (Person *person : people)
+    {
+        delete person;
+    }
+
     return 0;
 }
